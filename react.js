ff3416fa3616fd94920429250411e5092fa43f35
Jitendra Sharma <jitendravishvakarma708097@gmail.com>
2025-04-19 18:20:58 +0530

    This is a first commit

---
+++ react.js
@@ -0,0 +1,13 @@
+  
+
+  let name = "Jitendra Sharma";
+  
+  function Myfirstreact(){
+    return (
+        <h1>
+            kese aap ab from :{name}
+        </h1>
+    )
+  };
+
+  <Myfirstreact/>
